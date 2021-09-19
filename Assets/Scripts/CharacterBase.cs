d1fdafe51d9c49a64495d8c8529794052ce4f4cc
Rafael Costa <rafaelcostart2@gmail.com>
2021-09-19 17:20:22 -0300

    lvl up system

---
+++ Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class CharacterBase : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int currentLvl;
+    public int fullLife;
+    public int currentLife;
+    public int armoClass;
+    public int strength;
+    public int dexterity;
+    public int constitution;
+    public int intelligence;
+    public int wisdom;
+    public int charisma;
+    protected void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    protected void Update()
+    {
+        
+    }
+}
